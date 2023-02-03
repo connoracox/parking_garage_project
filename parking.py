0b7c9989d368687bd8ad1671715eb2a2e3a524d8
Abe <acec62392@gmail.com>
2023-02-03 12:41:21 -0800

    merging code

--- parking.py
+++ parking.py
@@ -4,7 +4,7 @@ class Parking():
             'quantity': 100,
             'parking': 1000,
             'price': 5.99,
-            'paid': True 
+            'paid': False 
         }
 
         self.list = []
@@ -24,24 +24,40 @@ class Parking():
         
     def payForParking(self):
         price = self.current_ticket['price']
-        cost = input('Please enter the amount owed: ($5.99) ')
-        if cost.isdigit():
-            cost = float(cost)
-            if cost == price:
-                print('Thank you, the ticket expires in 15 minutes. Please exit soon.')
-            elif cost > price:
-                refund = cost - price
-                print(f'Thank you, please wait for your change ${refund}. The ticket expires in 15 minutes. Please exit soon.')
+        amount_owed = price
+        while amount_owed != 0:
+            cost = input(f'Please enter the amount owed ${amount_owed}: ')
+            if cost.isdigit():
+                cost = float(cost)
+                if cost == amount_owed:
+                    print('Thank you, the ticket expires in 15 minutes. Please exit soon.')
+                    self.current_ticket['paid'] = True 
+                    break
+                elif cost > amount_owed:
+                    refund = cost - price
+                    print(f'Thank you, please wait for your change ${refund}. The ticket expires in 15 minutes. Please exit soon.')
+                    self.current_ticket['paid'] = True 
+                    break
+                elif cost < amount_owed:
+                    amount_owed -= cost
+                    print(f'Please pay the remaining amount ${amount_owed}: ')
             else:
-                amount_owed = price - cost
-                print(f'Please pay the remaining amount ${amount_owed}')
-                return
-        else:
-            print("Invalid input, please enter a number in digits.")
+                print("Invalid input, please enter a number in digits.")
 
     def show_list(self):
         for items in self.list:
+            print('Please take your ticket')
             print(f'Currently there are {items.new_amount} tickets and  {items.available_parking} parking spaces available.')
+
+    def leave_garage(self):
+        for ticket in self.current_ticket:
+            if 'paid' == True:
+                print('Thank you! Have a nice day!')
+            else:
+                input("Please pay your ticket amount:")
+                print("Thank you have a nice day!")
+            testing = self.list['new_amount'] + 1
+            print(f'{testing}')
 
     def main(self):
         while True:
@@ -54,7 +70,7 @@ class Parking():
                     garage.payForParking()
                 # elif user_selection == 3:
                 #     garage.
-                    return
+                    
             else:
                 print('Please value in digits ')
 
