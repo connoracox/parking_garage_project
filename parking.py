f3a926fdf069e81eae5d281b55de75717e5daa4e
Abe <acec62392@gmail.com>
2023-02-03 13:10:48 -0800

    final

--- parking.py
+++ parking.py
@@ -4,24 +4,18 @@ class Parking():
             'quantity': 100,
             'parking': 1000,
             'price': 5.99,
-            'paid': False 
+            'paid': False
         }
 
-        self.list = []
-
-
     def take_ticket(self):
-        quantity = self.current_ticket['quantity']
-        parking = self.current_ticket['parking']
-        ticket_count = 1
-        new_amount = quantity - ticket_count
-        available_parking = parking - ticket_count
-        new_ticket = Tickets(new_amount, available_parking)
-        self.list.append(new_ticket)
-        # print('Please take your ticket')
-        # print(f'There are now {new_amount} tickets available and {available_parking} parking spaces open.')
-        return
-        
+        while True:
+            self.current_ticket['quantity'] -= 1
+            self.current_ticket['parking'] -= 1
+            print('Please take your ticket')
+            print(
+                f"Currently there are {self.current_ticket['quantity']} tickets and {self.current_ticket['parking']} parking spaces available.")
+            return
+
     def payForParking(self):
         price = self.current_ticket['price']
         amount_owed = price
@@ -30,13 +24,15 @@ class Parking():
             if cost.isdigit():
                 cost = float(cost)
                 if cost == amount_owed:
-                    print('Thank you, the ticket expires in 15 minutes. Please exit soon.')
-                    self.current_ticket['paid'] = True 
+                    print(
+                        'Thank you, the ticket expires in 15 minutes. Please exit soon.')
+                    self.current_ticket['paid'] = True
                     break
                 elif cost > amount_owed:
                     refund = cost - price
-                    print(f'Thank you, please wait for your change ${refund}. The ticket expires in 15 minutes. Please exit soon.')
-                    self.current_ticket['paid'] = True 
+                    print(
+                        f'Thank you, please wait for your change ${refund}. The ticket expires in 15 minutes. Please exit soon.')
+                    self.current_ticket['paid'] = True
                     break
                 elif cost < amount_owed:
                     amount_owed -= cost
@@ -44,35 +40,38 @@ class Parking():
             else:
                 print("Invalid input, please enter a number in digits.")
 
-    def show_list(self):
-        for items in self.list:
-            print('Please take your ticket')
-            print(f'Currently there are {items.new_amount} tickets and  {items.available_parking} parking spaces available.')
-
     def leave_garage(self):
         for ticket in self.current_ticket:
-            if 'paid' == True:
+            if self.current_ticket['paid'] == True:
                 print('Thank you! Have a nice day!')
+
             else:
-                input("Please pay your ticket amount:")
+                print('Please pay for the ticket')
+                self.payForParking()
                 print("Thank you have a nice day!")
-            testing = self.list['new_amount'] + 1
-            print(f'{testing}')
+
+            self.current_ticket['parking'] += 1
+            print(f"Current number of spots: {self.current_ticket['parking']}")
+
+            self.current_ticket['quantity'] += 1
+            print(
+                f"Current number of tickets: {self.current_ticket['quantity']}")
+            return
 
     def main(self):
         while True:
-            user_selection = input('What would you like to do? Take a ticket (#1), Pay for parking (#2), Leave garage (#3). Please enter digit value: ')
+            user_selection = input(
+                'What would you like to do? Take a ticket (#1), Pay for parking (#2), Leave garage (#3). Please enter digit value: ')
             if user_selection.isdigit():
                 if user_selection == '1':
                     garage.take_ticket()
-                    garage.show_list()
                 elif user_selection == '2':
                     garage.payForParking()
-                # elif user_selection == 3:
-                #     garage.
-                    
+                elif user_selection == '3':
+                    garage.leave_garage()
+                    return
             else:
-                print('Please value in digits ')
+                print('Please enter value in digits. ')
 
 
 class Tickets():
@@ -81,7 +80,5 @@ class Tickets():
         self.available_parking = available_parking
 
 
-
 garage = Parking()
 garage.main()
-
